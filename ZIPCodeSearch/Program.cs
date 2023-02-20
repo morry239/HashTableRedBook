8e621e9ebfdf68c3cd34b3b21f3d9f67cb186ac8
morry239 <73179728+morry239@users.noreply.github.com>
2023-02-20 17:32:28 +0100

    Add files via upload

--- ZIPCodeSearch/Program.cs
+++ ZIPCodeSearch/Program.cs
@@ -1,21 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
 
-using System.Text;
-using IronXL;
-
-namespace ZIPCodeSearch
+namespace ZIPCodeSuche_RotesBuch
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Program prg = new Program();
-            string filePath = @"/Users/mamikawamura/Desktop/geo-data.csv";
-            TextReader r = File.OpenText(filePath);
-            Finder find = new Finder();
-            find.AddItem("","","");
-            find.Build(new StreamReader(args[0], Encoding.Default));
-            find.Classify(String.Empty);//dient zur Vorhersage der über Build-Methode erzeugten Regeln
+            var codeSearch = new ZIPCodeSearch();
+            codeSearch.Build(new StreamReader(args[0], Encoding.Default)); 
+            
+            Console.WriteLine($"Anwendung ZIPCodeSearch");
+
+            string value;
+            while ((value = Console.ReadLine()) != "")
+            {
+                var scanned = codeSearch.Classify(value);
+                Console.WriteLine($"Stadt: {codeSearch.Classify(value)} unbekannt");
+                Console.WriteLine($"Land:{codeSearch.Classify(value)}");
+            }
         }
+
+        
     }
 }
